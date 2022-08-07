841c77732d3382258e7b69563b61cc1e5f347d91
shhmabbey <abhernndz@gmail.com>
2022-08-06 18:15:24 -0700

    Nest React within frontend

--- frontend/entry.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-
-
-document.addEventListener("DOMContentLoaded",()=>{
-    console.log(html_element)
-    html_element = document.getElementById("root");
-    ReactDOM.render(<h1>React is working</h1>, html_element);
-})
---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Insta Pix is up and running!</h1>
+  );
+}
+
+export default App;
